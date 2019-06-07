d6b506cd23f0341754fefdafdfa667f2a93c969c
Owen Orrison <owenorrison@Owens-MacBook-Air.local>
2019-06-06 21:56:31 -0700

    react base set up. basic map working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import{ Map, TileLayer} from 'react-leaflet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapBoxTiles = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const mapBoxAttr = "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors";
+const mapCenter = [51.505, -0.09];
+const zoomLevel = 13;
+const id = 'mapid'
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 2,
+  }
+  }
+  render(){
+    const position = [this.state.lat, this.state.lng]
+      return (
+        <Map className="map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+        </Map>
+      )
+}
 }
 
 export default App;
